f2cb7c7474bbd772bb31c41817b9cfed02b39cec
youngxhui <youngxhui@gmail.com>
2017-06-23 11:48:44 +0800

    添加音乐，等级，积分

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame
 import random
 import time
+import math
 
 WIDTH = 480
 HEIGHT = 800
@@ -10,7 +11,11 @@ HEIGHT = 800
 SHOOT_PC = 0
 ENEMY_PC = 0
 
+enemy_add = 300
 player_shoot = 0
+
+score = 0
+level = 1
 
 pygame.init()
 
@@ -21,6 +26,8 @@ pygame.display.set_caption('飞机大战')
 background = pygame.image.load('resources/image/background.png').convert()
 
 plane_img = pygame.image.load('resources/image/shoot.png')
+
+shootMusic = pygame.mixer.Sound('resources/sound/bullet.mp3')
 
 
 # 子弹对象
@@ -79,8 +86,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.right += self.move
 
     def shoot(self, bullet_img):
+
         bullet = Bullet(bullet_img, self.rect.midtop)
         self.bullets.add(bullet)
+        shootMusic.play()
 
 
 # 敌机类
@@ -127,10 +136,13 @@ enemies_shoot_img = [plane_img.subsurface(pygame.Rect(267, 347, 57, 43)),
                      plane_img.subsurface(pygame.Rect(267, 296, 57, 43)),
                      plane_img.subsurface(pygame.Rect(930, 697, 57, 43))]
 
-enemies = pygame.sprite.Group()
-
 enemies_shoot = pygame.sprite.Group()
 
+backgroundMusic = pygame.mixer.music.load('resources/sound/game_music.mp3')
+pygame.mixer.music.play(-1, 0.0)
+
+enemy_shoot_sound = pygame.mixer.Sound('resources/sound/enemy3_down.mp3')
+enemy_shoot_sound.set_volume(1.0)
 RUN = True
 
 while RUN:
@@ -139,16 +151,16 @@ while RUN:
 
     if not player.is_hit:
         screen.blit(player.image[player.img_index], player.rect)
-        player.img_index = SHOOT_PC / 248
+        player.img_index = SHOOT_PC / 247
     else:
-        player.img_index = player_shoot / 248
+        player.img_index = player_shoot / 247
         screen.blit(player.image[player.img_index], player.rect)
         player_shoot += 31
-        if player_shoot > 1488:
+        if player_shoot > 495:
             RUN = False
             time.sleep(1)
 
-    if ENEMY_PC % 500 == 0:
+    if ENEMY_PC % enemy_add == 0:
         enemy_position = [random.randint(0, WIDTH - enemy_rect.width), 0]
         enemy = Enemy(enemy_img, enemies_shoot_img, enemy_position)
         enemies.add(enemy)
@@ -174,28 +186,40 @@ while RUN:
         if enemy.rect.top > HEIGHT:
             enemies.remove(enemy)
 
-    # for enemy_shoot in enemies_shoot:
-    #     if enemy_shoot.img_index == 0:
-    #         pass
-    #     if enemy_shoot.img_index > 7:
-    #         enemies_shoot.remove(enemy_shoot)
-    #         continue
-    #     screen.blit(enemy_shoot.shoot_imgs[enemy_shoot.img_index / 2])
-    #     enemy_shoot.img_index += 1
-    # enemies.draw(screen)
-
-    for enemy_down in enemies_shoot:
-        if enemy_down.shoot_index == 0:
+    for enemy_shoots in enemies_shoot:
+        if enemy_shoots.shoot_index == 0:
             pass
-        if enemy_down.shoot_index > 70:
-            enemies_shoot.remove(enemy_down)
+        if enemy_shoots.shoot_index > 70:
+            enemies_shoot.remove(enemy_shoots)
+            score += 100
             continue
-        screen.blit(enemy_down.shoot_imgs[enemy_down.shoot_index / 20], enemy_down.rect)
-        enemy_down.shoot_index += 1
+        screen.blit(enemy_shoots.shoot_imgs[enemy_shoots.shoot_index / 20], enemy_shoots.rect)
+        enemy_shoots.shoot_index += 1
+
     enemies.draw(screen)
 
+    # 分数显示
+    score_font = pygame.font.Font(None, 36)
+    score_text = score_font.render(str(score), True, (128, 128, 128))
+    text_rect = score_text.get_rect()
+    text_rect.topleft = [10, 10]
+    screen.blit(score_text, text_rect)
+
+    if score == 100 * (level ** 2 + level):
+        level += 1
+        enemy_add -= 25
+
+    # 等级显示
+    level_font = pygame.font.Font(None, 42)
+    level_text = level_font.render('Level ' + str(level), True, (128, 128, 128, 128))
+    level_rect = level_text.get_rect()
+    level_rect.midtop = [240, 10]
+    screen.blit(level_text, level_rect)
+
     enemies_is_shoot = pygame.sprite.groupcollide(enemies, player.bullets, 0.6, 0.8)
+
     for enemy_shoot in enemies_is_shoot:
+        enemy_shoot_sound.play()
         enemies_shoot.add(enemy_shoot)
 
     # 获取键盘按键
@@ -212,7 +236,7 @@ while RUN:
     if key_pressed[pygame.K_SPACE]:
         if SHOOT_PC % 495 == 0:
             player.shoot(bullet_img)
-        SHOOT_PC = SHOOT_PC + 100
+        SHOOT_PC = SHOOT_PC + 5
         if SHOOT_PC >= 495:
             SHOOT_PC = 0
 
@@ -224,20 +248,26 @@ while RUN:
             pygame.quit()
             exit()
 
+if not RUN:
+    pygame.mixer.music.stop()
+
 GAMEOVER = True
 
 # 加载gameOver图片
 
-gameOver = pygame.image.load('resources/image/gameover.png')
+gameOver_image = pygame.image.load('resources/image/gameover.png')
+
+# 加载GameOVer音乐
+
+gameOver_music = pygame.mixer.music.load('resources/sound/game_over.mp3')
+pygame.mixer.music.play(0, 0.0)
 
 while GAMEOVER:
     screen.fill(0)
-    screen.blit(gameOver, (0, 0))
+    screen.blit(gameOver_image, (0, 0))
 
     pygame.display.update()
 
-    # 退出程序
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    if pygame.mixer.music.get_busy() == 0:
+        pygame.quit()
+        exit()
